3911e862df2aeaad320357d9bb543cc36736c010
Kamye <andrej15ganzha@gmail.com>
2018-10-13 00:59:25 +0300

    navigation

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,71 @@
+/* ===================================================================
+ * Glint - Main JS
+ *
+ * ------------------------------------------------------------------- */
 $(function() {
+	"use strict";
+    
+  var cfg = {
+      scrollDuration : 800, // smoothscroll duration
+	},
+	
+	$WIN = $(window);
 
-	$('.parallax-window').parallax({imageSrc: '../img/hero-bg.jpg'});
+  //Add the User Agent to the <html>
+  // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
+  var doc = document.documentElement;
+	doc.setAttribute('data-useragent', navigator.userAgent);
+	
+ /* Menu on Scrolldown
+  * ------------------------------------------------------ */
+	var clMenuOnScrolldown = function() {
+        
+		var menuTrigger = $('.header-menu-toggle');
 
-});
+		$WIN.on('scroll', function() {
+
+			if ($WIN.scrollTop() > 150) {
+					menuTrigger.addClass('opaque');
+			}
+			else {
+					menuTrigger.removeClass('opaque');
+			}
+
+		});
+	};
+
+	/* OffCanvas Menu
+    * ------------------------------------------------------ */
+	 var clOffCanvas = function() {
+
+		var menuTrigger     = $('.header-menu-toggle'),
+				nav             = $('.header-nav'),
+				closeButton     = nav.find('.header-nav__close'),
+				siteBody        = $('body'),
+				mainContents    = $('section, footer');
+
+		// open-close menu by clicking on the menu icon
+		menuTrigger.on('click', function(e){
+				e.preventDefault();
+				// menuTrigger.toggleClass('is-clicked');
+				siteBody.toggleClass('menu-is-open');
+		});
+
+		// close menu by clicking the close button
+		closeButton.on('click', function(e){
+				e.preventDefault();
+				menuTrigger.trigger('click');	
+		});
+
+		// close menu clicking outside the menu itself
+		siteBody.on('click', function(e){
+				if( !$(e.target).is('.header-nav, .header-nav__content, .header-menu-toggle, .header-menu-toggle span') ) {
+						// menuTrigger.removeClass('is-clicked');
+						siteBody.removeClass('menu-is-open');
+				}
+		});
+
+};
+clOffCanvas();
+clMenuOnScrolldown();
+})
